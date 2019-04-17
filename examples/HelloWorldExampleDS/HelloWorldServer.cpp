5af9407c30c98b5c57150a52f9077e4371ecc9f3
Miguel Barro Otero <MiguelBarro@eprosima.com>
2019-04-17 16:52:43 +0200

    HelloWorld Example introduced. Example is operational. Warning in compilation due to fast-RTPS dell exports of LocatorList_t.

---
+++ examples/HelloWorldExampleDS/HelloWorldServer.cpp
@@ -0,0 +1,69 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file HelloWorldServer.cpp
+ *
+ */
+
+#include "HelloWorldServer.h"
+#include <fastrtps/participant/Participant.h>
+#include <fastrtps/attributes/ParticipantAttributes.h>
+
+#include <fastrtps/Domain.h>
+#include <fastrtps/utils/eClock.h>
+#include <fastrtps/utils/IPLocator.h>
+
+using namespace eprosima::fastrtps;
+using namespace eprosima::fastrtps::rtps;
+
+HelloWorldServer::HelloWorldServer():mp_participant(nullptr)
+{
+}
+
+bool HelloWorldServer::init()
+{
+    Locator_t server_address(LOCATOR_KIND_UDPv4, 65215);
+    IPLocator::setIPv4(server_address, 127, 0, 0, 1);
+
+    RemoteServerAttributes ratt;
+    ratt.ReadguidPrefix("4D.49.47.55.45.4c.5f.42.41.52.52.4f");
+    ratt.metatrafficUnicastLocatorList.push_back(server_address);
+
+    ParticipantAttributes PParam;
+    PParam.rtps.builtin.discoveryProtocol = PDPType_t::SERVER;
+    PParam.rtps.prefix = ratt.guidPrefix;
+    PParam.rtps.builtin.metatrafficUnicastLocatorList = ratt.metatrafficUnicastLocatorList;
+    PParam.rtps.builtin.domainId = 0;
+    PParam.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PParam.rtps.setName("Participant_sub");
+    mp_participant = Domain::createParticipant(PParam);
+    if(mp_participant==nullptr)
+        return false;
+
+    return true;
+}
+
+HelloWorldServer::~HelloWorldServer() {
+    // TODO Auto-generated destructor stub
+    Domain::removeParticipant(mp_participant);
+}
+
+
+void HelloWorldServer::run()
+{
+    std::cout << "Server running. Please press enter to stop the server" << std::endl;
+    std::cin.ignore();
+}
+
