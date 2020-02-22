a3590ba40063e10f716b24f4f33b48707b1b2cb0
Miguel Barro <miguelbarro@eprosima.com>
2020-02-22 11:55:51 +0100

    lease duration and liveliness test functional

--- test/python/test_8_lease_duration.py
+++ test/python/test_8_lease_duration.py
@@ -25,8 +25,8 @@ tmpdir = tempfile.mkdtemp()
 # print(f'temporary dir: {tmpdir}')
 
 # launch 
-proc_server = subprocess.Popen([discovery_path, xml_server_path])
-proc_client = subprocess.Popen([discovery_path, xml_client_path])
+proc_server = subprocess.Popen([discovery_path, xml_server_path], cwd=tmpdir)
+proc_client = subprocess.Popen([discovery_path, xml_client_path], cwd=tmpdir)
 
 # wait 3 seconds before killing the client
 try:
@@ -52,7 +52,24 @@ if not os.path.exists(snapshot):
 # load the file
 tree = ET.parse(snapshot)
 
-# TODO: parse the files and verify the out of process client demise is noticed
+# query the number of times clien3 appears on the first snapshot. It should be 3 (one for each participant in the server
+# config file) 
+nodes1 = tree.findall('./DS_Snapshot[1]//*[@name="client3"]')
+
+# query the number of times clien3 appears on the second snapshot. It should be none because the server should notify
+# all clients of client3 demise 
+nodes2 = tree.findall('./DS_Snapshot[2]//*[@name="client3"]')
+
+if not(len(nodes1) == 3 and len(nodes2) == 0): 
+    print('Snapshots show that lease duration is not working properly', file=sys.stderr)
+    sys.exit(2)
+
+# verify all participants on the server side share the same info
+proc_val = subprocess.run([discovery_path, snapshot])
+
+if proc_val.returncode: # 0 if everything goes fine
+    print(f'discovery info validation fails: returncode {proc_val.returncode}', file=sys.stderr) 
+    sys.exit(proc_val.returncode)
 
 # clean things up
 try:
