360a8af849f0508e7402cf16cf341ca79bf46775
Miguel Barro <miguelbarro@eprosima.com>
2020-02-24 09:35:43 +0100

    Solved test lexycographical comparisson issue. Avoid includes and sources installation.

--- test/python/test_08_lease_duration.py
+++ test/python/test_08_lease_duration.py
@@ -16,8 +16,8 @@ import subprocess, time, os, sys, tempfile, xml.etree.ElementTree as ET
 
 # compose the arguments
 discovery_path = sys.argv[1]     # '\\...\\discovery-server\\build64\\Debug\\discovery-server-1.1.0d.exe'
-xml_server_path = sys.argv[2]     # '\\...\\discovery-server\\resources\\xml\\test_9_WLP_server.xml'
-xml_client_path = sys.argv[3]     # '\\...\\discovery-server\\resources\\xml\\test_9_WLP_client.xml'
+xml_server_path = sys.argv[2]     # '\\...\\discovery-server\\resources\\xml\\test_08_lease_server.xml'
+xml_client_path = sys.argv[3]     # '\\...\\discovery-server\\resources\\xml\\test_08_lease_client.xml'
 
 # print(f'arguments: {discovery_path} {xml_server_path} {xml_client_path}')
 
@@ -26,11 +26,11 @@ tmpdir = tempfile.mkdtemp()
 
 # launch 
 proc_server = subprocess.Popen([discovery_path, xml_server_path], cwd=tmpdir)
-proc_client = subprocess.Popen([discovery_path, xml_client_path], cwd=tmpdir) 
+proc_client = subprocess.Popen([discovery_path, xml_client_path], cwd=tmpdir)
 
-# wait 4 seconds before killing the client
+# wait 3 seconds before killing the client
 try:
-    proc_client.wait(4)
+    proc_client.wait(3)
 except subprocess.TimeoutExpired:
     proc_client.kill()
 
@@ -42,7 +42,7 @@ if proc_server.returncode: # 0 if everything goes fine
     sys.exit(proc_server.returncode)
 
 # Verify if the file is there
-snapshot = os.path.join(tmpdir,'test_9_server_snapshot.xml')
+snapshot = os.path.join(tmpdir,'test_8_server_snapshot.xml')
 #print(f'Output snapshot file: {snapshot}')
 
 if not os.path.exists(snapshot):
@@ -52,15 +52,24 @@ if not os.path.exists(snapshot):
 # load the file
 tree = ET.parse(snapshot)
 
-# query the alive_count attributes on the first DS_Snapshot to verify all subscribers detected the publisher 
-nodes1 = tree.findall('./DS_Snapshot[1]//subscriber[@alive_count="1"]') 
+# query the number of times clien3 appears on the first snapshot. It should be 3 (one for each participant in the server
+# config file) 
+nodes1 = tree.findall('./DS_Snapshot[1]//*[@name="client3"]')
 
-# query the alive_count attributes on the second DS_Snapshot to verify all subscriber notice the publisher dead
-nodes2 = tree.findall('./DS_Snapshot[2]//subscriber[@alive_count="0"]') 
+# query the number of times clien3 appears on the second snapshot. It should be none because the server should notify
+# all clients of client3 demise 
+nodes2 = tree.findall('./DS_Snapshot[2]//*[@name="client3"]')
 
-if not(len(nodes1) == len(nodes2) == 2): 
-    print('Snapshots show the liveliness is not working properly', file=sys.stderr)
+if not(len(nodes1) == 3 and len(nodes2) == 0): 
+    print('Snapshots show that lease duration is not working properly', file=sys.stderr)
     sys.exit(2)
+
+# verify all participants on the server side share the same info
+proc_val = subprocess.run([discovery_path, snapshot])
+
+if proc_val.returncode: # 0 if everything goes fine
+    print(f'discovery info validation fails: returncode {proc_val.returncode}', file=sys.stderr) 
+    sys.exit(proc_val.returncode)
 
 # clean things up
 try:
@@ -70,5 +79,4 @@ except:
     print('Unable to remove the temporary files and dirs generated in the test.', file=sys.stderr)
     sys.exit(3)
 
-# success
-sys.exit(0)
+sys.exit(proc_server.returncode) # 0 if everything goes fine
