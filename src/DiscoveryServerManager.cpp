ffe7c2e2abe568f51b82324050b2925c48bf8826
cferreiragonz <carlosferreira@eprosima.com>
2024-04-22 14:45:30 +0200

    Refs #20629: Add GUIDLess tests
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- src/DiscoveryServerManager.cpp
+++ src/DiscoveryServerManager.cpp
@@ -1566,7 +1566,7 @@ void DiscoveryServerManager::MapServerInfo(
 
     if (prefix == c_GuidPrefix_Unknown)
     {
-        LOG_ERROR("Servers cannot have a framework provided prefix"); // at least for now
+        LOG_INFO("Guidless server, locators must be set directly from the XML");
         return;
     }
 
